1f4cdde836881f2a4c48c7313796e0f1a8961df4
Keegan Kelly <keegkelly2013@gmail.com>
2021-02-25 16:30:19 -0500

    defined stuff

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from qset_lib import Rover
 import move
 import lidar
 import dstar
+#define node grid
 
 def main():
     rover = Rover()
     x_target = float(input ("Target x coordinate"))
     y_target = float(input ("Target y coordinate"))
-
+    grid_x = 10
+    grid_y = 10
+    grid_res = 0.5
     lidar.updategrid()
     dstar.compute()
     move.movement()
+
     
