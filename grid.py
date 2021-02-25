99efb2fd8c430900711aea8c6c3b2fe7a663e339
Nikola Milosevic <nikolamilosevic441@gmail.com>
2021-02-25 16:47:33 -0500

    add grid.py

---
+++ grid.py
@@ -0,0 +1,11 @@
+
+class Grid:
+
+    def __init__(self, width, height):
+        self.array = [[0 for x in range(width)] for y in range(height)]
+        self.width = width
+        self.height = height
+
+    def index(self, x, y):
+        pass
+
