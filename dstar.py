b7866552b486aa1b69a35b068a37b362ec0eaca6
Daniel Sa <danielsa1901@gmail.com>
2021-02-26 11:36:44 -0500

    changes

--- dstar.py
+++ dstar.py
@@ -1,6 +1,27 @@
 from qset_lib import Rover
+import lidar
+import math
+#from queue import PriorityQueue
+
 
 rover=Rover()
+grid_edge_cost = 1
+
+
+def compute(x, y):
+    pass
+
+def heuristics(s_x, s_y):
+    pass
+    node_x = start_x - s_x
+    node_y = start_y - s_y
+    node_final = math.sqrt((node_x**2)+(node_y**2))
+
+    return node_final
+    
+def get_g(s1):
+    pass
+
 
 
 
--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import dstar
 
 def main():
     rover = Rover()
+    
     x_target = float(input ("Target x coordinate"))
     y_target = float(input ("Target y coordinate"))
+
     grid_x = 11
     grid_y = 11
+
     x_0 = round(grid_x/2)
     y_0 = round(grid_y/2)
+
     grid_res = 0.5
     grid = [[0 for x in range(grid_x)] for y in range(grid_y)]
+    
     lidar.updategrid(grid_res)
     dstar.compute(x_target, y_target)
     move.movement()
