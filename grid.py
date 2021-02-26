d02656c68f15145d0cd7b63b9617ddab54153d4b
Nikola Milosevic <nikolamilosevic441@gmail.com>
2021-02-26 13:42:14 -0500

    basic lidar

--- grid.py
+++ grid.py
@@ -1,10 +1,10 @@
 
 class Grid:
 
-    def __init__(self, width, height, resolution):
+    def __init__(self, width, height, resolution, default_value=0):
         assert width % 2 == 1, "width should be odd"
         assert height % 2 == 1, "height should be odd"
-        self.array = [[0 for x in range(width)] for y in range(height)]
+        self.array = [[default_value for x in range(width)] for y in range(height)]
         self.width = width
         self.height = height
         self.resolution = resolution
--- lidar.py
+++ lidar.py
@@ -1,14 +1,26 @@
 import math
+from grid import Grid
+
 
 # update_grid will read the lidar array and update the grid in place
 # It will return True if any changes were actually made, and False otherwise
-def update_grid(x, y, heading, lidar_array, grid):
+def update_grid(x, y, heading, laser_distances, grid):
     made_changes = False
 
-    for i, laser in enumerate(lidar_array):
-        angle = heading + math.pi * ((i - 7) / 28.0)
-        print(i, angle)
+    for i, distance in enumerate(laser_distances):
+        # angle is in radians
+        angle = heading * math.pi / 180.0 + math.pi * (i - 7) / 28.0
+
+        # heading of 0 deg = +X
+        # heading of 90 deg = +Y
+        world_x = distance * math.cos(angle) + x
+        world_y = distance * math.sin(angle) + y
+        grid_x = int(round(world_x * grid.resolution))
+        grid_y = int(round(world_y * grid.resolution))
+        grid.set_coordinate(grid_x, grid_y, float('inf'))
 
     return made_changes
 
-update_grid(0, 0, 0, range(15), 0)
+grid = Grid(99, 99, 0.5, default_value=0.0)
+update_grid(0, 0, 0, range(15), grid)
+print(grid.array)
