673340bb6f421b1bbc97e13a6e878a1004b94aea
Nikola Milosevic <nikolamilosevic441@gmail.com>
2021-02-26 13:07:34 -0500

    change the grid

--- grid.py
+++ grid.py
@@ -2,13 +2,15 @@
 class Grid:
 
     def __init__(self, width, height):
+        assert width % 2 == 1, "width should be odd"
+        assert height % 2 == 1, "height should be odd"
         self.array = [[0 for x in range(width)] for y in range(height)]
         self.width = width
         self.height = height
 
     def get_coordinate(self, x, y):
-        pass
+        return self.array[x + int(self.width / 2)][y + int(self.height / 2)]
 
     def set_coordinate(self, x, y, val):
-        pass
+        self.array[x + int(self.width / 2)][y + int(self.height / 2)] = val
 
--- main.py
+++ main.py
@@ -5,18 +5,14 @@ import dstar
 
 def main():
     rover = Rover()
-    
+
     x_target = float(input ("Target x coordinate"))
     y_target = float(input ("Target y coordinate"))
 
-    grid_x = 11
-    grid_y = 11
-
-    x_0 = round(grid_x/2)
-    y_0 = round(grid_y/2)
-
+    grid_width = 11
+    grid_height = 11
     grid_res = 0.5
-    grid = [[0 for x in range(grid_x)] for y in range(grid_y)]
+    grid = Grid(grid_width, grid_height)
     
     lidar.updategrid(grid_res)
     dstar.compute(x_target, y_target)
