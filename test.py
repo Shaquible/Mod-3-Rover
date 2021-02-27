e73b468d15a1515b819a7a452615aac70b2595b0
Keegan Kelly <keegkelly2013@gmail.com>
2021-02-26 20:37:00 -0500

    tested sucsess

--- test.py
+++ test.py
@@ -2,11 +2,14 @@ from qset_lib import Rover
 import math
 import time
 rover = Rover()
-while round(rover.x, 1) != 1-0.4:
-    rover.send_command(1,0)
+target = 10
+while round(rover.x, 1) < target -0.1:
+    
+    
+    rover.send_command(0.415,0)
     print(rover.x)
 rover.send_command(0,0)
-time.sleep(5)
+time.sleep(3)
 print(rover.x)
 
 #should use threading and wait to delay send commands but constantly check position to hault
