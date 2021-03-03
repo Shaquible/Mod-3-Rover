2be53142e6a010bfd11fb2e512fd1bb57add202b
Keegan Kelly <keegkelly2013@gmail.com>
2021-03-03 17:59:31 -0500

    move

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import dstar
 def main():
     rover = Rover()
 
-    #x_target = float(input("Target x coordinate: "))
-    #y_target = float(input("Target y coordinate: "))
+    x_target = float(input("Target x coordinate: "))
+    y_target = float(input("Target y coordinate: "))
 
     grid_width = 11
     grid_height = 11
@@ -16,9 +16,8 @@ def main():
     grid = Grid(grid_width, grid_height, grid_res, default_value=0.0)
 
     changed = lidar.update_grid(rover.x, rover.y, rover.heading, rover.laser_distances, grid)
-    #print(grid.array)
-    #print (changed)
-    #dstar.compute(x_target, y_target)
+    
+    dstar.compute(x_target, y_target)
     node_x = 6
     node_y = 8.5
     move.movement(node_x,node_y)
--- move.py
+++ move.py
@@ -1,31 +1,43 @@
 import math
 import time
+from qset_lib import Rover
 # will get next node in sequence from d*
 #use current position and target position to get neccacary heading
 #check current position against target to reach that position
-def movement(targetx, targety):
 #change heading
 #change target to actuall target from d*
+def movement(targetx, targety):
+    rover = Rover()
     delta_x = targetx - rover.x
-    delta_y = targety - rover.x
-    target_head = 180/math.pi * math.atan(delta_y / delta_x)
-    angularv = 0.25
+    delta_y = targety - rover.y
+    if delta_x == 0:
+        if delta_y > 0:
+            target_head = 90
+        if delta_y < 0:
+            target_head = 90
+    if delta_x != 0:
+        target_head = 180/math.pi * math.atan(delta_y / delta_x)
+    angularv = 0.6
     if rover.heading > target_head:
-        angularv = -angularv
+        angularv = -1 * angularv
 
     while round(rover.heading, 1) != round(target_head, 1):
         rover.send_command(0, angularv)
         print(rover.heading)
 
-    rover.send_command(0,-angularv)
-    time.sleep(0.5)
-    while round(rover.x, 1) < targetx and round(rover.y, 1) < targety:
+    rover.send_command(0,-0.0001)
+    rover.send_command(0,0)
+    time.sleep(2)
+    
+    rover.send_command(0,-0.1 * angularv)
+    while round(rover.x, 1) != targetx and round(rover.y, 1) != targety:
         rover.send_command(0.5,0)
         print(rover.x, rover.y)
-    rover.send_command(-0.5,0)
+    rover.send_command(-0.1,0)
 
     time.sleep(2)
-    print(rover.x,rover.y)
+    print(rover.x,rover.y,rover.heading)
+
 
 
 
