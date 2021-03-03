3ab2a5af9d6d563ef230862a699299e3b30ef6a9
Keegan Kelly <keegkelly2013@gmail.com>
2021-03-03 17:05:31 -0500

    implemented 1st move

--- move.py
+++ move.py
@@ -1,3 +1,31 @@
+import math
 # will get next node in sequence from d*
 #use current position and target position to get neccacary heading
 #check current position against target to reach that position
+def move(targetx, targety)
+#change heading
+#change target to actuall target from d*
+delta_x = targetx - rover.x
+delta_y = targety - rover.x
+target_head = 180/math.pi * math.atan(delta_y / delta_x)
+angularv = 0.25
+if rover.heading > target_head:
+    angularv = -angularv
+
+while round(rover.heading, 1) != round(target_head, 1)
+    rover.send_command(0, angularv)
+    print(rover.heading)
+
+rover.send_command(0,-angularv)
+time.sleep(0.5)
+while round(rover.x, 1) < targetx and round(rover.y, 1) < targety
+    rover.send_command(0.5,0)
+    print(rover.x, rover.y)
+rover.send_command(-0.5,0)
+
+time.sleep(2)
+print(rover.x,rover.y)
+
+
+
+
