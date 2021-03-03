1ef741f67acd3b3ff1884e48637b3c8bc99978d9
tera-bytes <tahira.m.hossain@gmail.com>
2021-03-03 08:29:12 -0500

    updated

--- dstar.py
+++ dstar.py
@@ -7,24 +7,26 @@ from queue import PriorityQueue
 rover=Rover()
 grid_edge_cost = 1
 
-def __init__(start_node,target_node):
+def __init__(self,start,target):
     pass
-    g = inf
-    km = 0
-    rhs = inf #0 for the goal node
+    self.start = start
+    self.g = inf
+    self.km = 0
+    self.rhs = inf #0 for the goal node
 
-def computeKey(s, start_node)
+def computeKey(s, start_node):
     pass
     key1 = min(g[s],rhs[s]) + heuristics(start_node,s)+km
     key2 = min(g[s],rhs[s])
+    return key1,key2
 
 def compute(x, y):
     pass
 
 def heuristics(s_x, s_y):
     pass
-    node_x = start_x - s_x
-    node_y = start_y - s_y
+    node_x = abs(start_x - s_x)
+    node_y = abs(start_y - s_y)
     node_final = math.sqrt((node_x**2)+(node_y**2))
     #must be 0 for start node
     return node_final
