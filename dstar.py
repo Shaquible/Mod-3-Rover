76f546903a8d0372f3c131da9e77f1b1d5571553
Daniel Sa <danielsa1901@gmail.com>
2021-03-03 16:47:16 -0500

    outline for dstar file

--- dstar.py
+++ dstar.py
@@ -1,40 +1,39 @@
-#from qset_lib import Rover
+from qset_lib import Rover
 import lidar
 import math
-<<<<<<< HEAD
 from Queue import PriorityQueue
-=======
 from grid import Grid
-from queue import PriorityQueue
->>>>>>> 1765c99eabfd35d17b8fdd0dae83550613170573
+from lidar import update_grid
+import heapq
 
 
-#rover=Rover()
+rover=Rover()
+q = PriorityQueue()
 grid_edge_cost = 1
 
-def __init__(self,start,goal):
+def __init__(self,start,goal): #initialize starting values
     pass
     self.start = start
+    self.postition = start
     self.goal = goal
     self.g = float('inf')
     self.km = 0
     self.rhs = float('inf') #0 for the goal node
 
-<<<<<<< HEAD
 def computeKey(s, start_node):
-=======
-def calculateKey(self,s):
->>>>>>> 1765c99eabfd35d17b8fdd0dae83550613170573
     pass
     key = [0,0]
     key[0] = min(g[s],rhs[s]) + heuristics(self.start,s)+km
     key[1] = min(g[s],rhs[s])
     return key
 
-def compute(x, y):
+def update_change(x, y): #find shortest path
     pass
 
-def heuristics(s_x, s_y):
+#function to check for change in edge cost
+# if function return true, change K_m to be h(s) from the start to the goal (looped).
+
+def heuristics(s_x, s_y): #distance from current node to start.
     pass
     node_x = abs(start_x - s_x)
     node_y = abs(start_y - s_y)
@@ -42,8 +41,33 @@ def heuristics(s_x, s_y):
     #must be 0 for start node
     return node_final
     
-def get_g(s):
+def get_g(s): # gets g value (distance from goal to node) for node s 
     pass
 
-def get_rhs(s):
+
+def get_rhs(s): #gets rhs value for node s (same process as g value).
     pass
+
+def pre(s): #finds predecessor node for current node s.
+    pass
+
+def suc(s): # find successor node for currne tnode s.
+    pass
+
+def update_vertex(s): #compare the g and rhs values for a node, check if node is on priority queue.
+    pass
+    if g(s) != rhs(s):
+        if g(s) > rhs(s):
+            pass
+            #set values equal to eachother
+            #update all predecessors of s.
+            #over consistent
+        if g(s) < rhs(s):
+            pass
+            #set g(s) equal to inf
+            #update all predecessors of s and s itself.
+            #under consistent
+
+def get_shortest_path():
+    pass
+
