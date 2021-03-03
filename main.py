f4951866882ddbbde84ee5c0d2c6eb55c09229bb
Keegan Kelly <keegkelly2013@gmail.com>
2021-03-03 17:17:00 -0500

    testing

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import dstar
 def main():
     rover = Rover()
 
-    x_target = float(input("Target x coordinate: "))
-    y_target = float(input("Target y coordinate: "))
+    #x_target = float(input("Target x coordinate: "))
+    #y_target = float(input("Target y coordinate: "))
 
     grid_width = 11
     grid_height = 11
@@ -18,7 +18,7 @@ def main():
     changed = lidar.update_grid(rover.x, rover.y, rover.heading, rover.laser_distances, grid)
     #print(grid.array)
     #print (changed)
-    dstar.compute(x_target, y_target)
+    #dstar.compute(x_target, y_target)
     node_x = 6
     node_y = 8.5
     move.movement(node_x,node_y)
