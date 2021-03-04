12bd2792c7edb1d639e79daa36f2315f2bc25511
Daniel Sa <danielsa1901@gmail.com>
2021-03-04 00:59:03 -0500

    temporarily made update neighbour function

--- dstar.py
+++ dstar.py
@@ -1,5 +1,4 @@
 from qset_lib import Rover
-import lidar
 import math
 from Queue import PriorityQueue
 from grid import Grid
@@ -11,6 +10,8 @@ rover=Rover()
 q = PriorityQueue()
 grid_edge_cost = 1
 
+# figure out how the grid works and how to implement it.
+
 def __init__(self,start,goal): #initialize starting values
     pass
     self.start = start
@@ -20,43 +21,43 @@ def __init__(self,start,goal): #initialize starting values
     self.km = 0
     self.rhs = float('inf') #0 for the goal node
 
-def computeKey(s, start_node):
+def computeKey(self, s, start_node):
     pass
-    key = [0,0]
-    key[0] = min(g[s],rhs[s]) + h(self.start,s)+km
-    key[1] = min(g[s],rhs[s])
+    self.key = [0,0]
+    self.key[0] = min(g[s],rhs[s]) + h(self.start,s)+km
+    self.key[1] = min(g[s],rhs[s])
     return key
 
-def update_change(x, y): #find shortest path
+def update_change(self, x, y): #find shortest path
     pass
 
 #function to check for change in edge cost
 # if function return true, change K_m to be h(s) from the start to the goal (looped).
 
-def heuristics(start, s2): #distance from current node to start.
+def heuristics(self, start, s2): #distance from current node to start.
     pass
     x1, y1 = start
     x2, y2 = s2
-    node_x = abs(x1 - x2)
-    node_y = abs(y1 - y2)
-    node_final = math.sqrt((node_x**2)+(node_y**2))
+    self.node_x = abs(x1 - x2)
+    self.node_y = abs(y1 - y2)
+    self.node_final = math.sqrt((node_x**2)+(node_y**2))
     #must be 0 for start node
     return node_final
     
-def get_g(s): # gets g value (distance from goal to node) for node s 
+def get_g(self, s): # gets g value (distance from goal to node) for node s 
     pass
 
 
-def get_rhs(s): #gets rhs value for node s (same process as g value).
+def get_rhs(self, s): #gets rhs value for node s (same process as g value).
     pass
 
-def pre(s): #finds predecessor node for current node s.
+def pre(self, s): #finds predecessor node for current node s.
     pass
 
-def suc(s): # find successor node for currne tnode s.
+def suc(self, s): # find successor node for currnet node s.
     pass
 
-def update_vertex(s): #compare the g and rhs values for a node, check if node is on priority queue.
+def update_vertex(self, s): #compare the g and rhs values for a node, check if node is on priority queue.
     pass
     if g(s) != rhs(s):
         if g(s) > rhs(s):
@@ -70,6 +71,31 @@ def update_vertex(s): #compare the g and rhs values for a node, check if node is
             #update all predecessors of s and s itself.
             #under consistent
 
-def get_shortest_path():
+def get_shortest_path(self):
     pass
 
+def update_neightbours(self ,s):
+    pass
+    self.neighbours = []
+    self.coordinate = grid.get_coordinate(s)
+    self.max_x = grid.width
+    self.max_y = grid.height
+    row , col = coordinate
+
+    if row > 0: #check to see if there is a row of nodes below the current node
+        self.neighbours.append(coordinate[row][col-1]) # adds bottom node to neighbour list
+        self.neighbours.append(coordinate[row-1][col-1]) # adds bottom left node to neighbour list
+        self.neighbours.append(coordinate[row+1][col-1]) # adds bottom right node to neighbour list
+
+    if row < max_x: #check to see if there is a row of nodes above the current node
+        self.neighbours.append(coordinate[row][col+1]) # adds top node to neighbour list
+        self.neighbours.append(coordinate[row-1][col+1]) # adds top left node to neighbour list
+        self.neighbours.append(coordinate[row+1][col+1]) # adds top right node to neighbour list
+    
+    if col > 0: #check to see if there is a colunm of nodes to the left of the current node
+        self.neighbours.append(coordinate[row+1][col]) # add right node to neighbour list
+        self.neighbours.append(coordinate[row-1][col]) # add left node to neighbour list
+    
+    
+    
+    
