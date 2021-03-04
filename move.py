88295614f4d5535fdf9fed99a474686909278137
Keegan Kelly <keegkelly2013@gmail.com>
2021-03-03 19:45:21 -0500

    fized angle function

--- move.py
+++ move.py
@@ -6,31 +6,39 @@ from qset_lib import Rover
 #check current position against target to reach that position
 #change heading
 #change target to actuall target from d*
+def heading(x, y)
+    if x == 0:
+    heading = 0
+    else:
+        heading = 180/math.pi * math.atan(abs(y / x))
+    if y <= 0 and x >= 0:
+        heading = -1 * heading
+    if y <= 0 and x <= 0:
+        heading = -180 - -1 * heading
+    if y >= 0 and x <= 0:
+        heading = 180 - heading    
+    return heading
+
 def movement(targetx, targety):
     rover = Rover()
-    delta_x = targetx - rover.x
-    delta_y = targety - rover.y
-    if delta_x == 0:
-        if delta_y > 0:
-            target_head = 90
-        if delta_y < 0:
-            target_head = 90
-    if delta_x != 0:
-        target_head = 180/math.pi * math.atan(delta_y / delta_x)
-        #need to convert from 0 to 360 to the +/- format of rover
-    angularv = 0.6
-    #this is not going to work wtih +/-
-    if rover.heading > target_head:
-        angularv = -1 * angularv
+    for i in range 2:
+        delta_x = targetx - rover.x
+        delta_y = targety - rover.y
+        target_head = heading(delta_x, delta_y)
+        angularv = 0.6
+        if rover.heading >= 0 and target_head >= 0:
+            if rover.heading > target_head:
+                angularv = -1 * angularv
+        if rover.heading < 0 and target_head < 0:
+            if rover.heading > target_head:
+                angularv = -1 * angularv
+        while round(rover.heading, 1) != round(target_head, 1):
+            rover.send_command(0, angularv)
+            print(rover.heading)
 
-    while round(rover.heading, 1) != round(target_head, 1):
-        rover.send_command(0, angularv)
-        print(rover.heading)
-
-    rover.send_command(0,-0.0001)
-    rover.send_command(0,0)
-    time.sleep(2)
-    #want to turn twice to readjust
+        rover.send_command(0,-0.001*angularv)
+        rover.send_command(0,0)
+    
     
     rover.send_command(0,-0.1 * angularv)
     while round(rover.x, 1) != targetx and round(rover.y, 1) != targety:
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-from qset_lib import Rover
 import math
-import time
-rover = Rover()
-target = 10
-while round(rover.x, 1) < target -0.1:
-    rover.send_command(0.415,0)
-    print(rover.x)
-rover.send_command(0,0)
-time.sleep(3)
-print(rover.x)
-
-#should use threading and wait to delay send commands but constantly check position to hault
+x = -2
+y = -1
+if x == 0:
+    heading = 0
+else:
+    heading = 180/math.pi * math.atan(abs(y / x))
+if y <= 0 and x >= 0:
+    heading = -1 * heading
+if y <= 0 and x <= 0:
+    heading = -180 - -1 * heading
+if y >= 0 and x <= 0:
+    heading = 180 - heading    
+print(heading)
