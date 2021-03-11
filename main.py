20dcbe3d3d15654ef157d1a5e843ce8e2415fcc8
tera-bytes <tahira.m.hossain@gmail.com>
2021-03-11 12:21:18 -0500

    added dstar

--- main.py
+++ main.py
@@ -15,21 +15,43 @@ def Main():
     grid_res = 0.5
     grid = Grid(grid_width, grid_height, grid_res, default_value=0.0)
 
-    changed = lidar.update_grid(rover.x, rover.y, rover.heading, rover.laser_distances, grid)
-    
-    if changed == True:
-       # dstar.compute(x_target, y_target)
-    #update d* queue
-    #get next target from d*
-    node_x = 4
-    node_y = 4
-    move.movement(node_x, node_y)
-    node_x = -6
-    node_y = 7
-    move.movement(node_x, node_y)
-
     start_node = (1,1)
     goal_node = (2,2)
+
     dlite = DStar(start_node,goal_node,grid)
+
+
+    current = dlite.start
+    dlite.get_shortest_path()
+    path = [dlite.start] # list of path nodes for testing
+    
+    while dlite.start != dlite.goal:
+            
+        #change start to neighbouring node with lowest cost
+        s_list = dlite.neighbours(dlite.start)
+        min_neighbour = float('inf')
+        for s in s_list:
+            if dlite.cost(dlite.start,s) + dlite.g[s[0]][s[1]] < min_neighbour:
+                min_neighbour = dlite.cost(dlite.start,s) + dlite.g[s[0]][s[1]]
+                min_node = s
+
+        dlite.start = min_node.copy()
+        path.append(dlite.start) #add to path
+        #get x and y coordinates from node
+        x = dlite.start[0] - int(len(dlite.world_grid[0])/2)
+        y = dlite.start[1] - int(len(dlite.world_grid)/2)
+        #move the rover to x and y
+        move.movement(x,y)
+
+        #call update_grid
+        changed = lidar.update_grid(rover.x, rover.y, rover.heading, rover.laser_distances, grid)
+        
+        #if there was a change in graph, set current = self.start
+        if changed:
+            dlite.km += dlite.heuristics(current,dlite.start)
+            current = dlite.start
+            #go thru all nodes with changes then update vertex for each?
+            dlite.update_vertex(current)
+            dlite.get_shortest_path()
     
 Main()
