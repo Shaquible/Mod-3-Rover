e669be49406a420d149712d8d7bb4eb54bf3fe0f
Keegan Kelly <keegkelly2013@gmail.com>
2021-03-12 19:21:04 -0500

    csv

--- csvoutput.py
+++ csvoutput.py
@@ -1,6 +1,5 @@
 import csv
-
 def csv(grid):
     with open(grid1.csv, 'w') as csvfile:
-        csvwrite = csv.writter(csvfile)
+        csvwriter = csv.writter(csvfile)
         csvwriter.writerows(grid)
