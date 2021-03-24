84f9856a280961159db69990d48dd8b28e66a42c
tera-bytes <tahira.m.hossain@gmail.com>
2021-03-24 14:57:47 -0400

    Cleaned code and comments

--- dstar.py
+++ dstar.py
@@ -1,7 +1,4 @@
-from qset_lib import Rover
-import lidar
 import math
-from lidar import update_grid
 import heapq
 from priority import PriorityQueue
 
@@ -34,7 +31,6 @@ class DStar:
 
             elif self.g[u[0]][u[1]] > self.rhs[u[0]][u[1]]: #if overconsistent
                 self.g[u[0]][u[1]] = self.rhs[u[0]][u[1]] #set g and rhs equal
-            
                 #loop thru all nodes s in neighbours list
                 for s in s_list:
                     self.update_vertex(s) #call update_vertex for each s
@@ -47,15 +43,13 @@ class DStar:
                 for s in s_list: #loop thru neighbours and call update_vertex
                     self.update_vertex(s)
 
-
+    #get key for sorting in queue
     def computeKey(self,s):
         key1 = min(self.g[s[0]][s[1]],self.rhs[s[0]][s[1]]) + self.heuristics(self.start,s)+self.km
         key2 = min(self.g[s[0]][s[1]],self.rhs[s[0]][s[1]])
         return (key1,key2)
 
-    #function to check for change in edge cost
-    #if function return true, change K_m to be h(s) from the start to the goal (looped).
-
+    #get euclidean distance estimate
     def heuristics(self,s,position): #distance from current node to start.
         x1,y1 = position
         x2,y2 = s
@@ -65,18 +59,17 @@ class DStar:
         
         return node_final
 
-
     #cost of movement from u to s
     def cost(self,u,s):
         x1,y1 = u
         x2,y2 = s
-        #note: this is temporary we prob have to check for obstacles another way
-        #if the value at node u or s is inf (i.e obstacle is detected), cost is inf 
+        
+        #if obstacle is at either node, cost is inf 
         if(self.sensed[x1][y1] == float('inf') or self.sensed[x2][y2] == float('inf')):
             return float('inf')
+        #if obstacles are adjacent to either node, cost is high
         elif(self.sensed[x1][y1] == 2000 or self.sensed[x2][y2] == 2000):
             return float(100000000000000)
-        #if no obstacles, cost will be one
         else:
             return self.heuristics(u,s)
 
@@ -90,38 +83,37 @@ class DStar:
         for s in nodes_near:
             if s[0] >= 0 and s[0] < len(self.world_grid[0]) and s[1] >= 0 and s[1] < len(self.world_grid):
                 filtered.append(s) #if it does, add to a new list with only existing neighbours
-        return filtered #return the new list
+        return filtered 
 
+    #update nodes in queue and their costs
     def update_vertex(self,u):
-        #compare the g and rhs values for a node, check if node is on priority queue.
-
         #if we are not on the goal node
         if u != self.goal:
             #get the neighbouring nodes of u
             nodes_near = self.neighbours(u)
             lowest_cost = float('inf')
 
-            #loop through all nodes s in the list of neighbours
             for s in nodes_near:
-                #if the movement cost from u to s added to the g(s) is lower than predicted minimum cost
+                #if total cost from u to s is lower than predicted min cost
                 if self.cost(u,s) + self.g[s[0]][s[1]] < lowest_cost:
-                    #update lowest cost to be that sum
+                    #update lowest cost 
                     lowest_cost = self.cost(u,s) + self.g[s[0]][s[1]]
             
             #once the final lowest lookahead g(u) is found, update rhs 
             self.rhs[u[0]][u[1]]=lowest_cost
-    
+        #remove from queue   
         self.open_set.delete(u)
-
+        
+        #if node cost has been changed, place back in queue
         if self.g[u[0]][u[1]] != self.rhs[u[0]][u[1]]:
             self.open_set.put(u,self.computeKey(u))
-
-            
+    
+    #get list of nodes near the start up to specified number of nodes ahead
     def sense_map(self,span):
         nodes_sensed = []
         row = len(self.world_grid)
         col = len(self.world_grid[0])
-
+        
         for i in range (-span,span+1):
             for j in range(-span,span+1):
                 if(self.start[0]+i >=0 and self.start[0]+i < row and self.start[1] + j >= 0 and self.start[1] + j < col):
